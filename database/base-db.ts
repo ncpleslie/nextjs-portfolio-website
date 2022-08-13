1ac12cf4ce7813b9116b06b5a9a29a9e022df00d
Nick Leslie <nleslie@jadeworld.com>
2022-08-13 20:29:07 +1200

    Move to env variables

--- database/base-db.ts
+++ database/base-db.ts
@@ -1,4 +1,4 @@
-import { FirebaseOptions, initializeApp } from "firebase/app";
+import { FirebaseOptions, initializeApp } from 'firebase/app'
 import {
   getFirestore,
   getDoc,
@@ -7,50 +7,50 @@ import {
   doc,
   query,
   Firestore,
-} from "@firebase/firestore/lite";
+} from '@firebase/firestore/lite'
 
-import firebaseConfig from "../firebase_config.json";
+import firebaseConfig from './firebase_config'
 
 export default abstract class BaseDB {
-  private db: Firestore;
+  private db: Firestore
 
   constructor() {
-    this.initializeDB();
+    this.initializeDB()
   }
 
   protected async getDocumentById<T>(
     collection: string,
     id: string
   ): Promise<{ id: string; data: T }> {
-    this.checkDBState();
+    this.checkDBState()
 
-    const docRef = doc(this.db, collection, id);
-    const snapshot = await getDoc(docRef);
+    const docRef = doc(this.db, collection, id)
+    const snapshot = await getDoc(docRef)
 
-    return { id: snapshot.id, data: snapshot.data() as T };
+    return { id: snapshot.id, data: snapshot.data() as T }
   }
 
   protected async getCollectionByName<T>(
     collectionName: string
   ): Promise<{ id: string; data: T }[]> {
-    this.checkDBState();
+    this.checkDBState()
 
-    const q = query(collection(this.db, collectionName));
-    const snapshot = await getDocs(q);
+    const q = query(collection(this.db, collectionName))
+    const snapshot = await getDocs(q)
 
     return snapshot.docs.map((doc) => {
-      return { id: doc.id, data: doc.data() as T };
-    });
+      return { id: doc.id, data: doc.data() as T }
+    })
   }
 
   private checkDBState(): void {
     if (!this.db) {
-      throw new Error("Database has not been initialized.");
+      throw new Error('Database has not been initialized.')
     }
   }
 
   private initializeDB(): void {
-    const app = initializeApp(firebaseConfig as FirebaseOptions);
-    this.db = getFirestore(app);
+    const app = initializeApp(firebaseConfig as FirebaseOptions)
+    this.db = getFirestore(app)
   }
 }
